bd7f0fbea8b0b31dc361a3d3c7d7b91821b11b6a
Kien Pham <kien@sendgrid.com>
2013-06-04 13:32:58 -0700

    Initial Commit

---
+++ SendGrid/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SendGrid
+//
+//  Created by Kien Pham on 6/4/13.
+//  Copyright (c) 2013 SendGrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SendGrid/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SendGrid
+//
+//  Created by Kien Pham on 6/4/13.
+//  Copyright (c) 2013 SendGrid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
